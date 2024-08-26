386fa300cdd3e7266683fcf72ba252671d90a5bc
Yadrick <ysaguirretr@est.utn.ac.cr>
2024-08-25 23:53:04 -0600

    Clase04

---
+++ src/routes/productos.ts
@@ -0,0 +1,13 @@
+import { Router } from "express";
+import ProductosController from "../controller/ProductosController";
+
+const routes= Router();
+
+routes.get("", ProductosController.getAll)
+routes.get("/getOne/:id",ProductosController.getOne)
+routes.post("", ProductosController.create)
+
+
+
+export default routes;
+
